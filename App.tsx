7f88caba7285c7dcc7abb25f6eed67a35ae701ab
Nicolas Descamps <nicolas.descamps@theodo.com>
2024-11-19 16:58:54 +0100

    Add counter

--- App.tsx
+++ App.tsx
@@ -1,11 +1,14 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { useState } from "react";
+import { Button, Pressable, StyleSheet, Text, View } from "react-native";
 
 export default function App() {
+  const [counter, setCounter] = useState(0);
   return (
     <View style={styles.container}>
-      <Text>Open up App.tsx to start working on your app!</Text>
-      <StatusBar style="auto" />
+      <Text>Le compteur est à {counter}.</Text>
+      <Pressable onPress={() => setCounter(counter + 1)}>
+        <Text style={styles.button}>+1</Text>
+      </Pressable>
     </View>
   );
 }
@@ -13,8 +16,15 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    backgroundColor: "#fff",
+    alignItems: "center",
+    justifyContent: "center",
+  },
+  button: {
+    backgroundColor: "lightblue",
+    paddingVertical: 10,
+    paddingHorizontal: 20,
+    borderRadius: 5,
+    color: "black",
   },
 });
